997066cdc5a81a5d8fe2d1844395f1c41b5b88de
gogobu <michael.bu@gmail.com>
2026-07-04 19:22:46 +1000

    re-base from the upstream zone_test branch

--- components/fujitsu-halcyon/esphome-fujitsu-halcyon.h
+++ components/fujitsu-halcyon/esphome-fujitsu-halcyon.h
@@ -11,7 +11,9 @@
 #include <esphome/components/uart/uart.h>
 #include <esphome/components/uart/uart_component_esp_idf.h>
 
+#if defined(USE_TZSP)
 #include <esphome/components/tzsp/tzsp.h>
+#endif
 
 #include "esphome-custom-button.h"
 #include "esphome-custom-number.h"
@@ -20,11 +22,16 @@
 
 namespace esphome::fujitsu_general_airstage_h_controller {
 
+#if defined(USE_TZSP)
 class FujitsuHalcyonController : public Component, public climate::Climate, public uart::UARTDevice, public tzsp::TZSPSender {
+#else
+class FujitsuHalcyonController : public Component, public climate::Climate, public uart::UARTDevice {
+#endif
     public:
         binary_sensor::BinarySensor* standby_sensor = new binary_sensor::BinarySensor();
         binary_sensor::BinarySensor* filter_sensor = new binary_sensor::BinarySensor();
         binary_sensor::BinarySensor* error_sensor = new binary_sensor::BinarySensor();
+        binary_sensor::BinarySensor* connected_sensor = new binary_sensor::BinarySensor();
         text_sensor::TextSensor* error_code_sensor = new text_sensor::TextSensor();
         text_sensor::TextSensor* initialization_sensor = new text_sensor::TextSensor();
         sensor::Sensor* remote_sensor = new sensor::Sensor();
@@ -34,6 +41,18 @@ class FujitsuHalcyonController : public Component, public climate::Climate, publ
         custom::CustomButton* advance_vertical_louver_button = new custom::CustomButton([this]() { this->controller->advance_vertical_louver(this->ignore_lock_); });
         custom::CustomButton* advance_horizontal_louver_button = new custom::CustomButton([this]() { this->controller->advance_horizontal_louver(this->ignore_lock_); });
         custom::CustomSwitch* use_sensor_switch = new custom::CustomSwitch([this](bool state) { return this->controller->use_sensor(state, this->ignore_lock_); });
+
+        std::array<custom::CustomSwitch*, fujitsu_general::airstage::h::MaxZone> zone_switches = [this] {
+            std::array<custom::CustomSwitch*, fujitsu_general::airstage::h::MaxZone> switches;
+
+            for (auto i = 0; i < switches.size(); i++)
+                switches[i] = new custom::CustomSwitch([this, i](bool state) { return this->controller->set_zone(i, state, this->ignore_lock_); });
+
+            return switches;
+        }();
+
+        custom::CustomSwitch* zone_group_day_switch = new custom::CustomSwitch([this](bool state) { return this->controller->set_zone_group_day(state, this->ignore_lock_); });
+        custom::CustomSwitch* zone_group_night_switch = new custom::CustomSwitch([this](bool state) { return this->controller->set_zone_group_night(state, this->ignore_lock_); });
 
         custom::CustomNumber* function = new custom::CustomNumber([this](float state) { return int(state); });
         custom::CustomNumber* function_value = new custom::CustomNumber([this](float state) { return int(state); });
@@ -49,20 +68,9 @@ class FujitsuHalcyonController : public Component, public climate::Climate, publ
                 this->controller->set_function(this->function->state, this->function_value->state, this->function_unit->state);
         });
 
-        std::array<custom::CustomSwitch*, fujitsu_general::airstage::h::MaximumZones> zone_switches = [this] {
-            std::array<custom::CustomSwitch*, fujitsu_general::airstage::h::MaximumZones> switches;
-
-            for (auto i = 0; i < switches.size(); i++)
-                switches[i] = new custom::CustomSwitch([this, i](bool state) { return this->controller->set_zone(i, state, this->ignore_lock_); });
-
-            return switches;
-        }();
-
-        custom::CustomSwitch* zone_group_day_switch = new custom::CustomSwitch([this](bool state) { return this->controller->set_zone_group_day(state, this->ignore_lock_); });
-        custom::CustomSwitch* zone_group_night_switch = new custom::CustomSwitch([this](bool state) { return this->controller->set_zone_group_night(state, this->ignore_lock_); });
-
         FujitsuHalcyonController(uart::IDFUARTComponent *parent, uint8_t controller_address) : uart::UARTDevice(parent), controller_address_(controller_address) {}
 
+        void loop() override;
         void setup() override;
         void dump_config() override;
         float get_setup_priority() const override { return esphome::setup_priority::DATA; }
@@ -91,6 +99,7 @@ class FujitsuHalcyonController : public Component, public climate::Climate, publ
         void update_from_device(const fujitsu_general::airstage::h::Packet& data);
         void update_from_device(const fujitsu_general::airstage::h::Function& data);
         void update_from_controller(const uint8_t address, const fujitsu_general::airstage::h::Config& data);
+        void on_initialization_stage(const fujitsu_general::airstage::h::InitializationStageEnum stage);
 
         void log_buffer(const char* dir, const uint8_t* buf, size_t length);
 
